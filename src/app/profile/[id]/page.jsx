6ee43da5b9d401ae4fb40de7af276bfae96e544b
t-r-a-c-y <tracytesi69@gmail.com>
2025-05-25 18:17:28 +0200

    new nextjs

--- src/app/profile/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function UserProfile({params}){
-    return(
-        <div className="text-center flex flex-col justify-center min-h-screen">
-            <h1 className="text-xl font-bold">Profile page</h1>
-            <hr />
-            <h1>Welcome to the {params.id} profile page</h1>
-        </div>
-    )
-}
--- src/app/profile/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage(){
-    return(
-        <div className="text-center flex flex-col justify-center min-h-screen">
-            <h1 className="text-xl font-bold">Profile page</h1>
-            <hr />
-            <h1>Welcome to the your profile page</h1>
-        </div>
-    )
-}
